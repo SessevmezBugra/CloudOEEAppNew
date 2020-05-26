feab51b306aa2afc1b2077a9fd0167477d8eb8e5
Bugra SESSEVMZ <sessevmezbugra@gmail.com>
2020-05-26 17:04:57 +0300

    company servislerinde ki int degerler longa cevrildi.

--- maindata-service/src/main/java/com/oee/repository/CompanyInfoRepository.java
+++ maindata-service/src/main/java/com/oee/repository/CompanyInfoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oee.entity.CompanyInfo;
 
-public interface CompanyInfoRepository extends JpaRepository<CompanyInfo, Integer>{
+public interface CompanyInfoRepository extends JpaRepository<CompanyInfo, Long>{
 	
 }
