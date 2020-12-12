87a40d8da40b932a42c6e157ee4690abac5a1434
Bugra SESSEVMZ <sessevmezbugra@gmail.com>
2020-12-12 20:56:42 +0300

    test

--- nginx-certbot/init-letsencrypt.sh
+++ nginx-certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(arcloudfactories.com auth.arcloudfactories.com)
+domains=(arcloudfactories.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sessevmezbugra@gmail.com" # Adding a valid address is strongly recommended
