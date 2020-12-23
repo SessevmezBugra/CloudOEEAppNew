b1fa984bd8eb416a6f2616560c9848c88a8f80ba
Bugra SESSEVMZ <sessevmezbugra@gmail.com>
2020-12-23 14:56:16 +0300

    bsessevmez

--- frontend-app/webapp/Component.js
+++ frontend-app/webapp/Component.js
@@ -27,7 +27,7 @@ sap.ui.define([
 						this.getRouter().getHashChanger().replaceHash("oeeapp");
 					} else {
 						this.keycloak.logout();
-						window.location.pathname="/index.html";
+						// window.location.pathname="/index.html";
 					}
 
 				} else {
